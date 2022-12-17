09a88cd546b698a99994c5d30c29c9d54632f331
VladmimirShtefan <shtefanvladimir14@gmail.com>
2022-12-17 13:37:54 +0700

    чистый проект с настройками

---
+++ skymarket/skymarket/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.template.defaulttags import url
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+# TODO здесь необходимо подклюючит нужные нам urls к проекту
+
+urlpatterns = [
+    path("api/admin/", admin.site.urls),
+    path("api/redoc-tasks/", include("redoc.urls")),
+    # url(r'^auth/', include('djoser.urls')),
+    url(r'^auth/', include('djoser.urls.jwt')),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
